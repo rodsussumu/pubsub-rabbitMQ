3c5a71298e47993c56098f035d0bc0c4a99dace0
Rodrigo Tanaka <rodsussumu@gmail.com>
2024-03-18 20:01:18 -0300

    initial commit

---
+++ publisher/src/test/java/com/sussumu/publisher/PublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sussumu.publisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublisherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
